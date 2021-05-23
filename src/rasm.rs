3ecd8a254fcff86c4feb8bbfe9106ee4cf286c5a
Bk <bigbk75@gmail.com>
2021-05-23 21:27:19 +0200

    Implemented more instructions

--- src/rasm.rs
+++ src/rasm.rs
@@ -28,7 +28,7 @@ impl SymbolTable {
         }
     }
     pub fn add_label(&mut self,label : String,line : usize) {
-        self.table.insert(label,line - 1);
+        self.table.insert(label,line);
         self.labels += 1;
     }
     pub fn get(&self,key : String) -> u16 {
@@ -44,25 +44,22 @@ impl SymbolTable {
 
 
 
-pub struct Code {
+pub struct Code{
     table : SymbolTable,
     pub code : Vec<Instruction>,
+    pub debug_info : Vec<(usize,String)>
 }
 impl Code {
-    pub fn new(table : SymbolTable,code : Vec<Instruction>) -> Self {
-        Self {table,code}
+    pub fn new(table : SymbolTable,code : Vec<Instruction>,debug_info : Vec<(usize,String)>) -> Self {
+        Self {table,code,debug_info}
     }
-    pub fn len(&self) -> u16 {
-        self.code.len() as u16
-    }
-    pub fn get(&self,i : usize) -> &Instruction {
-        &self.code[i]
+    pub fn get(&self,i : usize) -> (&Instruction,&(usize,String)) {
+        (&self.code[i],&self.debug_info[i])
     }
 }
 
 pub fn run(file : &Path) {
     let code = build_code(&file); 
-    println!("{:?}",code.code);
     exec::execute(code);
 }
 #[derive(Debug)]
@@ -70,12 +67,23 @@ pub enum Instruction {
     IO(bool),
     LDD(u16),
     LDM(i16),
+    LDI(u16), //Indirect addressing
+    LDR(i16), //Load into index register <imm> 
+    LDX(u16), //Indexed addressing 
+    MOV     , //ACC -> IX lol 
     SUBM(i16),
     SUBA(u16),
     STO(u16),
     ADDM(i16),
     ADDA(u16),
     INC(bool), //true:ACC, false:IX
+    DEC(bool), //true:ACC, false:IX
+    LSL(u16),  //logical shift left 
+    LSR(u16),  //logical shift right
+    XOR(u16), //XOR with contents of address 
+    XORM(i16),//XOR with immediate value
+    OR(i16),  //XOR with immediate value 
+    ORA(u16), //Xor with contents of address
     CMPA(u16),
     CMPM(i16),
     JPEM(u16),
@@ -95,12 +103,17 @@ impl Instruction {
         if &opcode == "STO" {
             return Self::STO(addr)
         }
+        //
         match opcode.chars().nth(0).unwrap() {
             'L' => Instruction::load_instruction(opcode,addr),
             'S' => Instruction::SUBA(addr),
             'A' => Instruction::ADDA(addr),
             'I' => Instruction::INC(addr == 0), //Acc is 0 in symtable
+            'D' => Instruction::DEC(addr == 0),
             'C' => Instruction::CMPA(addr),
+            'X' => Instruction::XOR(addr),
+            'O' => Instruction::ORA(addr),
+            'M' => Instruction::MOV,
             'J' => Instruction::jmp_instruction_addr(opcode,addr),
             'E' => Instruction::END,
              _  => Instruction::UNKNOWN,
@@ -109,11 +122,24 @@ impl Instruction {
     }
     pub fn with_imm(opcode : String, imm : i16) -> Self {
         let first_char = opcode.chars().nth(0).unwrap();
+        
         match first_char {
-            'L' => Self::load_instruction(opcode,imm as u16),
+            'L' => {
+                if opcode.chars().nth(1).unwrap() == 'S' {
+                    if opcode.chars().nth(2).unwrap() == 'R' {
+                        Instruction::LSR(imm as u16)
+                    }else {
+                        Instruction::LSL(imm as u16)
+                    }   
+                }else {
+                    Self::load_instruction(opcode,imm as u16)
+                }
+            },
             'S' => Instruction::SUBM(imm),
             'A' => Instruction::ADDM(imm),
             'C' => Instruction::CMPM(imm),
+            'X' => Instruction::XORM(imm),
+            'O' => Instruction::OR(imm),
             'J' => Self::jmp_instruction_imm(opcode,imm as u16),
              _  => Instruction::UNKNOWN,
 
@@ -123,6 +149,9 @@ impl Instruction {
         match opcode.chars().last().expect("Error on load instruction") {
             'D' => Instruction::LDD(data),
             'M' => Instruction::LDM(data as i16),
+            'R' => Instruction::LDR(data as i16),
+            'I' => Instruction::LDI(data),
+            'X' => Instruction::LDX(data),
              _  => Instruction::UNKNOWN,
         }
     }
@@ -146,6 +175,7 @@ impl Instruction {
 fn build_code(file : &Path) -> Code {
     let mut table = SymbolTable::new();
     let mut code = Vec::new();
+    let mut debug_info = Vec::new();
     let raw_bytes = std::fs::read(file).unwrap();
     let file_contents = String::from_utf8_lossy(&raw_bytes);
     for (index,line) in file_contents.lines().enumerate() {
@@ -161,13 +191,13 @@ fn build_code(file : &Path) -> Code {
         let mut line = line.trim();
         if let Some(n) = line.find(";") {
             if n == 0 {
-                eprintln!("Comment found on line {}",index);
                 continue;
             }
             line = line.split_at(n).0;
         }
         if line == "END" {
             code.push(Instruction::END);
+            debug_info.push((index,line.to_string()));
             continue;
         }
         if line.ends_with(":") {
@@ -179,6 +209,7 @@ fn build_code(file : &Path) -> Code {
         //tabxxxspace---
         if line.ends_with("IN") || line.ends_with("OUT") {
             code.push(Instruction::IO(line.ends_with("IN")));
+            debug_info.push((index,line.to_string()));
             continue;
         }
     
@@ -203,6 +234,7 @@ fn build_code(file : &Path) -> Code {
                 code.push(Instruction::with_imm(opcode,p));
                 table.min_addr = min(table.min_addr, p as u16);
                 table.max_addr = max(table.max_addr, p as u16);
+                debug_info.push((index,line.to_string()));
                 continue;
             }
         }
@@ -217,11 +249,11 @@ fn build_code(file : &Path) -> Code {
             };
             let imm = imm.expect("Error while parsing immediate value");
             code.push(Instruction::with_imm(opcode.to_string(), imm));
-
+            debug_info.push((index,line.to_string()));
             continue;
         }
         code.push(Instruction::new(opcode,table.get(ident.to_string())));
-
+        debug_info.push((index,line.to_string()));
     }
-    Code::new(table,code)
+    Code::new(table,code,debug_info)
 }
--- src/rasm/exec.rs
+++ src/rasm/exec.rs
@@ -1,24 +1,66 @@
 use super::{Code,Instruction};
 use std::fmt::{Display,Result,Formatter};
+use std::ops::{Index,IndexMut};
+pub struct Memory {
+    min_addr : u16,
+    mem : Vec<i16>,
+}
 
-struct CPU {
+impl Memory {
+    pub fn new(min_addr : u16) -> Self {
+        Memory {
+            min_addr,
+            mem : Vec::with_capacity(16),
+        }
+    }
+}
+impl Index<usize> for Memory {
+    type Output = i16;
+    fn index(&self, index: usize) -> &Self::Output {
+        if index as u16 > self.min_addr {
+            let i = index - self.min_addr as usize;
+            &self.mem[i]
+        }else {
+            &self.mem[index]
+        }
+    }
+}
+impl IndexMut<usize> for Memory {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+         if index as u16 > self.min_addr {
+            let i = index - self.min_addr as usize;
+            if i >= self.mem.len() {
+                self.mem.resize(i + 1,0);
+            }
+            
+            &mut self.mem[i]
+        }else {
+            if index >= self.mem.len() {
+                self.mem.resize(index + 1, 0);
+            }
+            &mut self.mem[index]
+        }
+       
+    }
+}
+pub struct CPU {
     acc : i16,
     ix : i16,
     pub pc : u16,
     done : bool,
     flag_register : Flags,
-    memory : [i16;10], 
+    pub memory : Memory, 
 }
 
 impl CPU {
-    pub fn new() -> Self {
+    pub fn new(min_addr : u16) -> Self {
         CPU {
             acc : 0,
             ix  : 0,
             pc  : 0,
             done : false,
             flag_register : Flags::new(),
-            memory : [0;10],
+            memory : Memory::new(min_addr),
         }
     }
     pub fn done(&self) -> bool {
@@ -27,22 +69,66 @@ impl CPU {
     pub fn execute(&mut self,instr : &Instruction) {
         use Instruction::*;
         self.pc += 1;
-        match instr {
-            &LDD(addr) => self.ldd(addr),
-            &LDM(imm)  => self.ldm(imm),
-            &IO(b)     => self.io(b),
-            &SUBM(imm) => self.subm(imm),
-            &SUBA(addr) => self.suba(addr),
-            &STO(addr)  => self.sto(addr),
-            &ADDM(imm)  => self.addm(imm),
-            &ADDA(addr) => self.adda(addr),
-            &INC(b)     => self.inc(b),
-            &CMPA(addr)  => self.cmpa(addr),
-            &CMPM(imm)   => self.cmpm(imm),
+        match *instr {
+            LDD(addr) => self.ldd(addr),
+            LDM(imm)  => self.ldm(imm),
+            LDI(addr) => self.ldi(addr),
+            LDR(imm)  => self.ldr(imm),
+            LDX(addr) => self.ldx(addr),
+            MOV       => self.ix = self.acc,
+            IO(b)     => self.io(b),
+            SUBM(imm) => self.subm(imm),
+            SUBA(addr) => self.suba(addr),
+            STO(addr)  => self.sto(addr),
+            ADDM(imm)  => self.addm(imm),
+            ADDA(addr) => self.adda(addr),
+            INC(b)     => self.addn(b,1),
+            DEC(b)     => self.addn(b,-1),
+            LSL(imm)   => self.lsl(imm),
+            LSR(imm)   => self.lsr(imm),
+            XORM(imm)  => self.xorm(imm),
+            XOR(addr)  => self.xora(addr),
+            OR(imm)    => self.or(imm),
+            ORA(addr)  => self.ora(addr),
+            CMPA(addr)  => self.cmpa(addr),
+            CMPM(imm)   => self.cmpm(imm),
             END         => self.end(),
             UNKNOWN     => eprintln!("Unkown command!"),
             _           => self.jmp(instr),
         }
+    }
+    fn lsl(&mut self, n : u16) {
+        self.flag_register.set_flag(FLAGS::C,(self.acc as u16 & 0x8000u16) != 0 );
+        self.acc <<= n;
+    }
+    fn lsr(&mut self, n : u16) {
+        self.flag_register.set_flag(FLAGS::C, (self.acc & 0x1) != 0);
+        self.acc >>= n;
+    }
+    fn ldi(&mut self,addr : u16) {
+        let d = self.memory[addr as usize];
+        self.acc = self.memory[d as usize];
+    }
+    fn ldr(&mut self,imm : i16) {
+        self.ix = imm;
+    }
+    fn ldx(&mut self,addr : u16) {
+        self.acc = self.memory[(addr + self.ix as u16) as usize];
+    }
+    fn xorm(&mut self, imm : i16) {
+        self.flag_register.set_flags(Some(self.acc ^ imm));
+        self.acc ^= imm;
+    }
+    fn xora(&mut self, addr : u16) {
+        self.xorm(self.memory[addr as usize]);
+    }
+    fn or(&mut self, imm : i16) {
+        self.flag_register.set_flags(Some(self.acc | imm));
+        self.acc |= imm;
+        
+    }
+    fn ora(&mut self, addr : u16) {
+        self.or(self.memory[addr as usize]);
     }
     fn ldd(&mut self,addr : u16) {
         self.acc = self.memory[addr as usize];
@@ -51,12 +137,9 @@ impl CPU {
         self.acc = imm;
     }
     fn io(&mut self, inp : bool) {
-        use std::io::{self,Write};
+        use std::io;
         if inp {
             
-            let stdio = io::stdout();
-            let mut handle = stdio.lock();
-            handle.write_all(b">").unwrap();
             let mut buffer = String::new();
             io::stdin().read_line(&mut buffer).unwrap();
             self.acc = buffer.chars().nth(0).unwrap() as u32 as i16;
@@ -85,11 +168,11 @@ impl CPU {
     fn adda(&mut self, addr : u16) {
         self.addm(self.memory[addr as usize]);
     }
-    fn inc(&mut self, acc : bool) {
+    fn addn(&mut self, acc : bool, n : i16) {
         if acc {
-            self.acc += 1;
+            self.acc += n;
         }else {
-            self.ix += 1;
+            self.ix += n;
         }
     }
     fn cmpm(&mut self,imm : i16) {
@@ -106,36 +189,20 @@ impl CPU {
     }
     fn jmp(&mut self, instr : &Instruction) {
         let ni = match instr {
-            &Instruction::JPEM(x) => {
+            &Instruction::JPEM(x) | &Instruction::JPEA(x) => {
                 if self.flag_register.get_flag(FLAGS::Z) {
                     x
                 }else {
                     self.pc 
                 }
             },
-            &Instruction::JPNM(x) => {
+            &Instruction::JPNM(x) | &Instruction::JPNA(x) => {
                 if self.flag_register.get_flag(FLAGS::Z) {
                     self.pc
                 }else {
                     x
                 }
             },
-            &Instruction::JPEA(x) => {
-                if self.flag_register.get_flag(FLAGS::Z) {
-                    self.memory[x as usize] as u16
-                }else {
-                    self.pc
-                }
-
-            },
-            &Instruction::JPNA(x) => {
-                if self.flag_register
-                    .get_flag(FLAGS::Z) {
-                        self.pc
-                    }else {
-                        self.memory[x as usize] as u16
-                    }
-            }
             _ => unreachable!()
 
         };
@@ -214,33 +281,33 @@ impl Flags {
    }
 
 }
-//find some use for this
-impl Display for Flags {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        //  _________
-        // | N V Z C |
-        // | 1 1 1 1 |
-        //  ---------
-       let line1 =      " _________";
-       let line2 =      " ---------";
-       let l2 =         "| N V Z C |";
-       let l3 = format!("| {} {} {} {} |",
-                self.get_flag(FLAGS::N) as u8,self.get_flag(FLAGS::V) as u8,
-                self.get_flag(FLAGS::Z) as u8,self.get_flag(FLAGS::C) as u8
-        );
-       write!(f,"{}\n{}\n{}\n{}",line1,l2,l3,line2)
-
-    }
-}
 pub fn execute(code : Code ) {
     use std::io;
-    let mut cpu = CPU::new();
-    while !cpu.done() {
-        let instr = code.get(cpu.pc as usize);
+    let mut cpu = CPU::new(code.table.min_addr);
+    let mut check_input = true;
+    'main : while !cpu.done() {
+        let (instr,actual_code) = code.get(cpu.pc as usize);
+        println!("-----------    Instruction Executing : {}   ------------",actual_code.1);
         cpu.execute(&instr);
         println!("{}",cpu);
-
-        let mut buf = String::new();
-        io::stdin().read_line(&mut buf).unwrap();
+        if check_input {
+            'input : loop {
+                let mut buf = String::new();
+                println!(r"
+Press:
+   enter: to step 1 instruction
+   q to quit
+   c to continue until end");
+                io::stdin().read_line(&mut buf).unwrap();
+                if buf.trim().is_empty() {
+                    break 'input
+                }
+                match buf.chars().nth(0).unwrap() {
+                    'q' => break 'main,
+                    'c' => {check_input = false; break 'input},
+                    _  => println!("Unrecognized command"),
+                };
+            }
+        }
     }
 }
